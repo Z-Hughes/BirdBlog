e18d95dcc24481b2836e2218dda9faf7e671bce4
Zach Hughes <zach.hughes79@gmail.com>
2022-04-22 15:31:21 -0400

    Creating individual post pages dynamically with templating

--- app.js
+++ app.js
@@ -13,42 +13,66 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-app.get("/", (req, res) =>{
-    res.render("home", {
-      startingContent: homeStartingContent, 
-      posts: posts
-    });
+app.get("/", (req, res) => {
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts
+  });
 })
 
-app.get("/about", (req, res) =>{
-  res.render("about", {about: aboutContent});
+app.get("/about", (req, res) => {
+  res.render("about", {
+    about: aboutContent
+  });
 })
 
-app.get("/contact", (req, res) =>{
-  res.render("contact", {contact: contactContent});
+app.get("/contact", (req, res) => {
+  res.render("contact", {
+    contact: contactContent
+  });
 })
 
-app.get("/compose", (req, res) =>{
+app.get("/compose", (req, res) => {
   res.render("compose");
 })
 
-app.post("/compose", (req,res) =>{
-    const post = {
-      title: req.body.postTitle,
-      content: req.body.postBody
-    };
-    posts.push(post);
-    res.redirect("/");
+app.post("/compose", (req, res) => {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  posts.push(post);
+  res.redirect("/");
+})
+
+// Creating individual post pages dynamically with templating
+// This also matches post title and requested title despite casing or dashes
+
+app.get("/post/:test", (req, res) => {
+  let reqTitle = req.params.test.toLowerCase();
+  reqTitle = reqTitle.split(' ').join('-');
+  posts.forEach((post) => {
+    let storedTitle = post.title.toLowerCase();
+    storedTitle = storedTitle.split(' ').join('-');
+
+    if (storedTitle === reqTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    }
+  })
 })
 
 
 
 
 
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
